7fc678fb75ba5ac48a5c6a19815b36600cf2aa9d
Fabien <cacrfabfr@gmail.com>
2021-11-03 14:08:01 +0100

    replaced setTimeout by onstransitioned function

--- 00-Full-Page/js/main.js
+++ 00-Full-Page/js/main.js
@@ -7,10 +7,8 @@ function initNavigation() {
     link.addEventListener('mouseleave', e => {
       // Add class to the nav link
       link.classList.add('animate-out');
-      setTimeout(() => {
-        // Remove class of the nav link
-        link.classList.remove('animate-out')
-      }, 300);
+      // Remove class of the nav link at the end of the animation
+      link.ontransitionend = () => link.classList.remove('animate-out');
     })
   })
 }
