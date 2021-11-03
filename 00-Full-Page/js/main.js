693667b0951f40c960f4b8bd1310c77ef728e46c
Fabien <cacrfabfr@gmail.com>
2021-11-03 16:52:26 +0100

    finished header and fixed animation for Safari users

--- 00-Full-Page/js/main.js
+++ 00-Full-Page/js/main.js
@@ -50,7 +50,47 @@ function moveImages(e) {
   const { offsetX, offsetY, target } = e;
   const { clientWidth, clientHeight } = target;
 
-  console.log(offsetX, offsetY, clientWidth, clientHeight);
+  // Get 0 0 in the center of the screen
+  const xPos = (offsetX / clientWidth) - 0.5;
+  const yPos = (offsetY / clientHeight) - 0.5;
+
+  const leftImages = gsap.utils.toArray('.hg__left .hg__image');
+  const rightImages = gsap.utils.toArray('.hg__right .hg__image');
+
+
+  const modifier = (index) => index * 1.2 + 0.5;
+
+  // Move the 3 images on the left
+  leftImages.forEach((image, index) => {
+    gsap.to(image, {
+      duration: 1.4,
+      x: xPos * 20 * modifier(index),
+      y: yPos * 30 * modifier(index),
+      rotationY: -(xPos * 35),
+      rotationX: -(yPos * 10),
+      ease: 'power3.out'
+    });
+  });
+
+  // Move the 3 images on the right
+  rightImages.forEach((image, index) => {
+    gsap.to(image, {
+      duration: 1.4,
+      x: xPos * 20 * modifier(index),
+      y: -yPos * 30 * modifier(index),
+      rotationY: -(xPos * 35),
+      rotationX: -(yPos * 10),
+      ease: 'power3.out'
+    });
+  });
+
+  // Move the background circle
+  gsap.to('.decor__circle', {
+    duration: 1.7,
+    x: 100 * xPos,
+    y: 120 * yPos,
+    ease: 'power4.out'
+  })
   
 }
 
