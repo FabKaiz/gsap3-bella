0f9927bf6def211dc9ead104999faea462930bf4
Fabien <cacrfabfr@gmail.com>
2021-11-03 13:59:20 +0100

    started navigation animations

--- 00-Full-Page/js/main.js
+++ 00-Full-Page/js/main.js
@@ -1,8 +1,23 @@
 gsap.registerPlugin(ScrollTrigger);
 
+function initNavigation() {
+
+  const mainNavLinks = gsap.utils.toArray('.main-nav a');
+  mainNavLinks.forEach(link => {
+    link.addEventListener('mouseleave', e => {
+      // Add class to the nav link
+      link.classList.add('animate-out');
+      setTimeout(() => {
+        // Remove class of the nav link
+        link.classList.remove('animate-out')
+      }, 300);
+    })
+  })
+}
+
 function init(){
     
-    // start here
+    initNavigation();
 
 }
 
