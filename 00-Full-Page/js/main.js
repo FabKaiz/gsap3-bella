fb98f91d2542296d8d7037c44ac719a39e9d3680
Fabien <cacrfabfr@gmail.com>
2021-11-03 14:26:02 +0100

    navigation fading and moving on scroll

--- 00-Full-Page/js/main.js
+++ 00-Full-Page/js/main.js
@@ -3,6 +3,8 @@ gsap.registerPlugin(ScrollTrigger);
 function initNavigation() {
 
   const mainNavLinks = gsap.utils.toArray('.main-nav a');
+  const mainNavLinksRev = gsap.utils.toArray('.main-nav a').reverse();
+
   mainNavLinks.forEach(link => {
     link.addEventListener('mouseleave', e => {
       // Add class to the nav link
@@ -12,12 +14,30 @@ function initNavigation() {
     })
   })
 
+  function navAnimation(direction) {
+
+    const scrollingDown = direction === 1;
+    const links = scrollingDown ? mainNavLinks : mainNavLinksRev;
+
+    return gsap.to(links, {
+      duration: 0.3,
+      stagger: 0.05,
+      autoAlpha: () => scrollingDown ? 0 : 1,
+      y: () => scrollingDown ? 20 : 0,
+      ease: 'power4.out'
+    })
+  }
+
+  // Moving the burger, text, and fading logo
   ScrollTrigger.create({
     start: 100,
+    end: 'bottom bottom-=20',
     toggleClass: {
       targets: 'body',
       className: 'has-scrolled'
     },
+    onEnter: ({direction}) => navAnimation(direction),
+    onLeaveBack: ({direction}) => navAnimation(direction),
     markers: true
   })
 }
