14ad5a1ce7a0cf7e55608ed1c4c9d7eef645fb81
Fabien <cacrfabfr@gmail.com>
2021-11-04 17:06:15 +0100

    gallery done, added media queries for mobile

--- 02-Reveal-Gallery/js/main.js
+++ 02-Reveal-Gallery/js/main.js
@@ -1,8 +1,10 @@
 gsap.registerPlugin(ScrollTrigger);
 
+// Galery Sections
+const sections = document.querySelectorAll('.rg__column');
+
 function initHoverReveal() {
 
-  const sections = document.querySelectorAll('.rg__column');
   sections.forEach(section => {
     
     // Get the component for the animation
@@ -43,12 +45,11 @@ function createHoverReveal(e) {
   });
 
   if (e.type === 'mouseenter') {
+
     tl
       .to([mask, imageBlock, textMask, textP], { yPercent: 0 })
       .to(text, { y: -getTextHeight(textCopy) / 2 }, 0)
       .to(image, { duration: 1.1, scale: 1 }, 0);
-
-
 
   } else if (e.type === 'mouseleave') {
 
@@ -58,19 +59,67 @@ function createHoverReveal(e) {
       .to(text, { y: 0 }, 0)
       .to(image, { duration: 1.1, scale: 1.2 }, 0);
 
+  }
 
+  return tl;
+}
+
+// function init(){
+
+//   initHoverReveal();
+
+// }
+
+// window.addEventListener('load', function(){
+//     init();
+// });
+
+// define a breakpoint
+const mq = window.matchMedia("(min-width: 768px)");
+
+// Add a change listner to this breakpoint
+mq.addEventListener('change', () => handleWidthChange());
+
+// First page load
+handleWidthChange();
+
+// Reset all props
+function resetProps(elements) {
+
+  gsap.killTweensOf('*');
+
+  if (elements.length) {
+    elements.forEach(el => {
+      el && gsap.set(el, {clearProps: 'all'});
+    })
+  }
+}
+
+// Media query change
+function handleWidthChange() {
+  
+  // Check if we are on the right breakpoint
+  if (mq.matches) {
+
+    // Setup hover animation
+    initHoverReveal();
+
+  } else {
+
+    // Width is less then 768px
+    console.log("We are on mobile");
+
+    // Remove event listeners on each section
+    sections.forEach(section => {
+
+      section.removeEventListener('mouseenter', createHoverReveal);
+      section.removeEventListener('mouseleave', createHoverReveal);
+
+      const { imageBlock, mask, text, textCopy, textMask, textP, image } = section;
+      resetProps([imageBlock, mask, text, textCopy, textMask, textP, image])
+
+
+    });
 
   }
-  return tl;
-
 }
-
-function init(){
-
-  initHoverReveal();
-
-}
-
-window.addEventListener('load', function(){
-    init();
-});
