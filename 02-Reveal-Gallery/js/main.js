eb3bb71f9ded3cb628f6a872f82b421dbdb5ec96
Fabien <cacrfabfr@gmail.com>
2021-11-04 16:33:55 +0100

    scale and text apear on hover effect

--- 02-Reveal-Gallery/js/main.js
+++ 02-Reveal-Gallery/js/main.js
@@ -8,10 +8,16 @@ function initHoverReveal() {
     // Get the component for the animation
     section.imageBlock = section.querySelector('.rg__image');
     section.mask = section.querySelector('.rg__image--mask');
+    section.text = section.querySelector('.rg__text');
+    section.textCopy = section.querySelector('.rg__text--copy');
+    section.textMask = section.querySelector('.rg__text--mask');
+    section.textP = section.querySelector('.rg__text--copy p');
+    section.image = section.querySelector('.rg__image img');
 
     // Reset the inital position
-    gsap.set(section.imageBlock, { yPercent: -101 });
-    gsap.set(section.mask, { yPercent: -101 });
+    gsap.set([section.imageBlock, section.textMask], { yPercent: -101 });
+    gsap.set([section.mask, section.textP], { yPercent: 100 });
+    gsap.set(section.image, { scale: 1.2 });
 
     //add event listeners to each section
     section.addEventListener('mouseenter', createHoverReveal);
@@ -21,9 +27,13 @@ function initHoverReveal() {
   })
 }
 
+function getTextHeight(textCopy) {
+  return textCopy.clientHeight;
+}
+
 function createHoverReveal(e) {
 
-  const { imageBlock, mask } = e.target;
+  const { imageBlock, mask, text, textCopy, textMask, textP, image } = e.target;
 
   const tl = gsap.timeline({
     defaults: {
@@ -33,12 +43,21 @@ function createHoverReveal(e) {
   });
 
   if (e.type === 'mouseenter') {
-    tl.to([mask, imageBlock], { yPercent: 0 });
+    tl
+      .to([mask, imageBlock, textMask, textP], { yPercent: 0 })
+      .to(text, { y: -getTextHeight(textCopy) / 2 }, 0)
+      .to(image, { duration: 1.1, scale: 1 }, 0);
+
+
 
   } else if (e.type === 'mouseleave') {
 
-    tl.to(mask, { yPercent: 100 });
-    tl.to(imageBlock, { yPercent: -101 }, 0);
+    tl
+      .to([mask, textP], { yPercent: 100 })
+      .to([imageBlock, textMask], { yPercent: -101 }, 0)
+      .to(text, { y: 0 }, 0)
+      .to(image, { duration: 1.1, scale: 1.2 }, 0);
+
 
 
   }
