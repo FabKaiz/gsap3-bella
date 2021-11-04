21f43d63a2859f435a3eef78d70cd9a94e5388f7
Fabien <cacrfabfr@gmail.com>
2021-11-04 15:39:28 +0100

    reveal galery reset position

--- 02-Reveal-Gallery/js/main.js
+++ 02-Reveal-Gallery/js/main.js
@@ -1,8 +1,35 @@
 gsap.registerPlugin(ScrollTrigger);
 
-function init(){
+function initHoverReveal() {
+
+  const sections = document.querySelectorAll('.rg__column');
+  sections.forEach(section => {
     
-    // start here
+    // Get the component for the animation
+    const imageBlock = section.querySelector('.rg__image');
+    const mask = section.querySelector('.rg__image--mask');
+
+    // Reset the inital position
+    gsap.set(imageBlock, { yPercent: -101 });
+    gsap.set(mask, { yPercent: -101 });
+
+    //add event listeners to each section
+    section.addEventListener('mouseenter', createHoverReveal);
+    section.addEventListener('mouseleave', createHoverReveal);
+
+
+  })
+}
+
+function createHoverReveal(e) {
+
+  console.log(e.type);
+
+}
+
+function init(){
+
+  initHoverReveal();
 
 }
 
