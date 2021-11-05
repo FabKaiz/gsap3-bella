989b3fd6b30732f2e0131699941a019d2ce98b3f
Fabien <cacrfabfr@gmail.com>
2021-11-05 11:15:26 +0100

    portfolio mouseleave

--- 03-Portfolio-Hover/js/main.js
+++ 03-Portfolio-Hover/js/main.js
@@ -27,21 +27,26 @@ function createPortfolioHover(e){
         .set(lInside, { backgroundImage: `url(${imagelarge})`})
         .set(sInside, { backgroundImage: `url(${imagesmall})`})
         // Fade in images
-        .to([largeImage, smallImage], {duration: 0.7, autoAlpha: 1})
+        .to([largeImage, smallImage], { duration: 0.7, autoAlpha: 1 })
         // All siblings to white and fade out
         .to(allSiblings, { color: '#fff', autoAlpha: 0.2 }, 0)
         // Active link to white
         .to(e.target, { color: '#fff', autoAlpha: 1 }, 0)
         // Update page background color
-        .to(pageBackground, { backgroundColor: color, ease: 'none' }, 0)
+        .to(pageBackground, { backgroundColor: color, ease: 'none' }, 0);
 
  
     } else if(e.type === 'mouseleave'){
- 
+
+      const tl = gsap.timeline();
+      tl
         // Fade out images
+        .to([largeImage, smallImage], { autoAlpha: 0 })
         // All links back to black
+        .to(allLinks, { color: '#000', autoAlpha: 1 }, 0)
         // Change background color back to default #ACB7AE
- 
+        .to(pageBackground, { backgroundColor: '#ACB7AE', ease: 'none' }, 0);
+
     }
  
 }
