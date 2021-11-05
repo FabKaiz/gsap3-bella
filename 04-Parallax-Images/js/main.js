9225d9998a1d18d90b3d42d9fbe52b3e0eca2925
Fabien <cacrfabfr@gmail.com>
2021-11-05 11:49:21 +0100

    parallax section

--- 04-Parallax-Images/js/main.js
+++ 04-Parallax-Images/js/main.js
@@ -2,10 +2,26 @@ gsap.registerPlugin(ScrollTrigger);
 
 function init(){
 
-  
+  // Select all sections .with-parallax
+  gsap.utils.toArray('.with-parallax').forEach(section => {
 
+    // Get the image
+    const image = section.querySelector('img');
+
+    // Create tween for the image
+    gsap.to(image, {
+      yPercent: 20,
+      ease: 'none',
+      scrollTrigger: {
+        trigger: section,
+        start: 'top bottom',
+        scrub: true,
+      }
+    })
+
+  })
 }
 
 window.addEventListener('load', function(){
-    init();
+  init();
 });
