7aa4d8ce783e486a7fbde5d84d101bb92d7abb3a
Fabien <cacrfabfr@gmail.com>
2021-11-08 13:17:40 +0100

    added imagesloaded that update the loading progress bar

--- 05-Intro-Animation/js/main.js
+++ 05-Intro-Animation/js/main.js
@@ -16,7 +16,41 @@ gsap.set(loader, { autoAlpha: 1 })
 gsap.set(loaderInner, { scaleY: 0.005, transformOrigin: 'bottom' });
 
 // Make a tween that scales the laoder
-gsap.to(progressBar, { scaleX: 0, ease: 'none', transformOrigin: 'right' })
+const progressTween = gsap.to(progressBar, { paused: true, scaleX: 0, ease: 'none', transformOrigin: 'right' });
+
+// Setup variables
+// https://codepen.io/desandro/pen/hlzaw
+let loadedImageCount = 0, imageCount;
+const container = select('#main');
+
+// Setup Imagesloaded
+const imgLoad = imagesLoaded( container );
+imageCount = imgLoad.images.length;
+
+
+// Set the initial progress to 0
+updateProgress(0);
+
+// triggered after each item is loaded
+imgLoad.on( 'progress', function() {
+  // increase the number of loaded images
+  loadedImageCount++;
+  // update progress
+  updateProgress( loadedImageCount );
+});
+
+// update the progress of our progressBar tween
+function updateProgress( value ) {
+  // console.log(value/imageCount)
+  // tween progress bar tween to the right value
+  gsap.to(progressTween, {progress: value/imageCount, duration: 0.3, ease: 'power1.out'})
+}
+
+// do whatever you want when all images are loaded
+imgLoad.on( 'done', function( instance ) {
+  // we will simply init our loader animation onComplete
+  gsap.set(progressBar, {autoAlpha: 0, onComplete: initLoader});
+});
 
 const initLoader = () => {
 
@@ -37,11 +71,12 @@ const initLoader = () => {
   });
 
   tlLoaderIn
-  .set(loaderContent, { autoAlpha: 1 })
-    .from(loaderInner, {
-      scaleY: 0,
-      transformOrigin: 'bottom'
-    }, 0.2)
+    .set(loaderContent, { autoAlpha: 1 })
+    .to(loaderInner, {
+      scaleY: 1,
+      transformOrigin: 'bottom',
+      ease: 'power1.inOut'
+    })
     .addLabel('revealImage')
     .from(mask, { yPercent: 100 }, 'revealImage-=0.6')
     .from(image, { yPercent: -90 }, 'revealImage-=0.6')
