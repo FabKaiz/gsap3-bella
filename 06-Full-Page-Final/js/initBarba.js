82f8a5722a8f1d45fc3c691b28b991c332d3bc81
Fabien <cacrfabfr@gmail.com>
2021-11-10 15:12:46 +0100

    Complete refactor for barbaJS page transition to work on every page

---
+++ 06-Full-Page-Final/js/initBarba.js
@@ -0,0 +1,147 @@
+const select = (e) => document.querySelector(e);
+const selectAll = (e) => document.querySelectorAll(e);
+
+const updateBodyColor = (color) => {
+  // gsap.to('.fill-background', { backgroundColor: color, ease: 'none'});
+  document.documentElement.style.setProperty("--bcg-fill-color", color);
+};
+
+const getTextHeight = (textCopy) => {
+  return textCopy.clientHeight;
+};
+
+function getPortfolioOffset(clientY) {
+  return -(select(".portfolio__categories").clientHeight - clientY);
+}
+
+function createPortfolioMove(e) {
+  const largeImage = select(".portfolio__image--l");
+  const smallImage = select(".portfolio__image--s");
+
+
+  const { clientY } = e;
+
+  // move large image
+  gsap.to(largeImage, {
+    duration: 1.2,
+    y: getPortfolioOffset(clientY) / 5,
+    ease: "power3.out",
+  });
+
+  // move small image
+  gsap.to(smallImage, {
+    duration: 1.5,
+    y: -getPortfolioOffset(clientY) / 3,
+    ease: "power3.out",
+  });
+}
+
+function portfolioHover() {
+  // Initialize Portfolio hover;
+  const allLinks = gsap.utils.toArray(".portfolio__categories a");
+  const largeImage = select(".portfolio__image--l");
+  const smallImage = select(".portfolio__image--s");
+  const lInside = select(".portfolio__image--l .image_inside");
+  const sInside = select(".portfolio__image--s .image_inside");
+  
+  // create hover effect for each portfolio navigation item
+  function createPortfolioHover(e) {
+
+    if (e.type === "mouseenter") {
+      // change images to the right urls
+      // fade in images
+      // all siblings to white and fade out
+      // active link to white
+      // update page background color
+      
+      const { color, imagelarge, imagesmall } = e.target.dataset;
+      const allSiblings = allLinks.filter((item) => item !== e.target);
+      const tl = gsap.timeline({
+        onStart: () => updateBodyColor(color),
+      });
+      tl.set(lInside, { backgroundImage: `url(${imagelarge})` })
+      .set(sInside, { backgroundImage: `url(${imagesmall})` })
+      .to([largeImage, smallImage], { autoAlpha: 1 })
+      .to(allSiblings, { color: "#fff", autoAlpha: 0.2 }, 0)
+      .to(e.target, { color: "#fff", autoAlpha: 1 }, 0);
+    } else if (e.type === "mouseleave") {
+      // fade out images
+      // all links back to black
+      // change background color back to default
+  
+      const tl = gsap.timeline({
+        onStart: () => updateBodyColor("#ACB7AE"),
+      });
+      tl.to([largeImage, smallImage], { autoAlpha: 0 }).to(
+        allLinks,
+        { color: "#000000", autoAlpha: 1 },
+        0
+      );
+    }
+  }
+
+  allLinks.forEach((link) => {
+    link.addEventListener("mouseenter", (e) => createPortfolioHover(e));
+    link.addEventListener("mouseleave", (e) => createPortfolioHover(e));
+    link.addEventListener("mousemove", (e) => createPortfolioMove(e));
+
+  });
+}
+
+function createHoverReveal(e) {
+
+  const { imageBlock, mask, text, textCopy, textMask, textP, image, dataset } =
+    e.target;
+
+  const { color } = dataset;
+
+  let tl = gsap.timeline({
+    defaults: {
+      duration: 1,
+      ease: "power4.out",
+    },
+  });
+
+  if (e.type === "mouseenter") {
+    tl.to([mask, imageBlock, textMask, textP], {
+      yPercent: 0,
+      onStart: () => updateBodyColor(color),
+    })
+      .to(text, { y: () => -getTextHeight(textCopy) / 2 }, 0)
+      .to(image, { duration: 1.1, scale: 1 }, 0);
+  } else if (e.type === "mouseleave") {
+    tl.to([mask, textP], { yPercent: 100 })
+      .to([imageBlock, textMask], { yPercent: -101 }, 0)
+      .to(text, { y: 0 }, 0)
+      .to(image, { scale: 1.2 }, 0);
+  }
+
+  return tl;
+}
+
+function hoverReveal() {
+
+  // Initialize the hover reveal after changing page
+  const sections = selectAll(".rg__column");
+  sections.forEach((section) => {
+    // get componenets for animation
+    section.imageBlock = section.querySelector(".rg__image");
+    section.image = section.querySelector(".rg__image img");
+    section.mask = section.querySelector(".rg__image--mask");
+    section.text = section.querySelector(".rg__text");
+    section.textCopy = section.querySelector(".rg__text--copy");
+    section.textMask = section.querySelector(".rg__text--mask");
+    section.textP = section.querySelector(".rg__text--copy p");
+    
+    // reset the initial position
+    gsap.set([section.imageBlock, section.textMask], { yPercent: -101 });
+    gsap.set([section.mask, section.textP], { yPercent: 100 });
+    gsap.set(section.image, { scale: 1.2 });
+    
+    // add event listeners to each section
+    section.addEventListener("mouseenter", createHoverReveal);
+    section.addEventListener("mouseleave", createHoverReveal);
+  });
+}
+
+export { hoverReveal, portfolioHover, updateBodyColor };
--- 06-Full-Page-Final/js/main.js
+++ 06-Full-Page-Final/js/main.js
@@ -1,19 +1,11 @@
 gsap.registerPlugin(ScrollTrigger);
+import { hoverReveal, portfolioHover, updateBodyColor } from './initBarba.js'
 
 let bodyScrollBar;
 
+// Query selector shortcut
 const select = (e) => document.querySelector(e);
 const selectAll = (e) => document.querySelectorAll(e);
-
-const sections = selectAll(".rg__column");
-
-// create hover effect for each portfolio navigation item
-const allLinks = gsap.utils.toArray(".portfolio__categories a");
-const pageBackground = select(".fill-background");
-const largeImage = select(".portfolio__image--l");
-const smallImage = select(".portfolio__image--s");
-const lInside = select(".portfolio__image--l .image_inside");
-const sInside = select(".portfolio__image--s .image_inside");
 
 const loader = select(".loader");
 const loaderInner = select(".loader .inner");
@@ -139,6 +131,20 @@ function initPageTransitions() {
   });
 
   barba.init({
+    views: [ // Use views to enbale Javascript or plugins on specific pages before or after transition
+      {
+        namespace: 'home',
+        beforeEnter: () => {
+        },
+        afterEnter: () => {
+          hoverReveal();
+          portfolioHover();
+          
+        },
+        beforeLeave: () => {
+        }
+      },
+    ],
     transitions: [
       {
         once() {
@@ -210,20 +216,10 @@ function initContent() {
   initSmoothScrollbar();
   initNavigation();
   initHeaderTilt();
-  initHoverReveal();
-  initPortfolioHover();
   initImageParallax();
   initPinSteps();
   initScrollTo();
 }
-
-const updateBodyColor = (color) => {
-  // gsap.to('.fill-background', { backgroundColor: color, ease: 'none'});
-  document.documentElement.style.setProperty("--bcg-fill-color", color);
-};
-const getTextHeight = (textCopy) => {
-  return textCopy.clientHeight;
-};
 
 // Smoooth Scrollbar
 function initSmoothScrollbar() {
@@ -346,126 +342,6 @@ function moveImages(e) {
   });
 }
 
-// Reveal Gallery
-function initHoverReveal() {
-  sections.forEach((section) => {
-    // get componenets for animation
-    section.imageBlock = section.querySelector(".rg__image");
-    section.image = section.querySelector(".rg__image img");
-    section.mask = section.querySelector(".rg__image--mask");
-    section.text = section.querySelector(".rg__text");
-    section.textCopy = section.querySelector(".rg__text--copy");
-    section.textMask = section.querySelector(".rg__text--mask");
-    section.textP = section.querySelector(".rg__text--copy p");
-
-    // reset the initial position
-    gsap.set([section.imageBlock, section.textMask], { yPercent: -101 });
-    gsap.set([section.mask, section.textP], { yPercent: 100 });
-    gsap.set(section.image, { scale: 1.2 });
-
-    // add event listeners to each section
-    section.addEventListener("mouseenter", createHoverReveal);
-    section.addEventListener("mouseleave", createHoverReveal);
-  });
-}
-
-function createHoverReveal(e) {
-  // console.log(e.type);
-
-  const { imageBlock, mask, text, textCopy, textMask, textP, image, dataset } =
-    e.target;
-
-  const { color } = dataset;
-
-  let tl = gsap.timeline({
-    defaults: {
-      duration: 1,
-      ease: "power4.out",
-    },
-  });
-
-  if (e.type === "mouseenter") {
-    tl.to([mask, imageBlock, textMask, textP], {
-      yPercent: 0,
-      onStart: () => updateBodyColor(color),
-    })
-      .to(text, { y: () => -getTextHeight(textCopy) / 2 }, 0)
-      .to(image, { duration: 1.1, scale: 1 }, 0);
-  } else if (e.type === "mouseleave") {
-    tl.to([mask, textP], { yPercent: 100 })
-      .to([imageBlock, textMask], { yPercent: -101 }, 0)
-      .to(text, { y: 0 }, 0)
-      .to(image, { scale: 1.2 }, 0);
-  }
-
-  return tl;
-}
-
-// Portfolio Hover
-function initPortfolioHover() {
-  allLinks.forEach((link) => {
-    link.addEventListener("mouseenter", createPortfolioHover);
-    link.addEventListener("mouseleave", createPortfolioHover);
-    link.addEventListener("mousemove", createPortfolioMove);
-  });
-}
-
-function createPortfolioHover(e) {
-  if (e.type === "mouseenter") {
-    // change images to the right urls
-    // fade in images
-    // all siblings to white and fade out
-    // active link to white
-    // update page background color
-
-    const { color, imagelarge, imagesmall } = e.target.dataset;
-    const allSiblings = allLinks.filter((item) => item !== e.target);
-    const tl = gsap.timeline({
-      onStart: () => updateBodyColor(color),
-    });
-    tl.set(lInside, { backgroundImage: `url(${imagelarge})` })
-      .set(sInside, { backgroundImage: `url(${imagesmall})` })
-      .to([largeImage, smallImage], { autoAlpha: 1 })
-      .to(allSiblings, { color: "#fff", autoAlpha: 0.2 }, 0)
-      .to(e.target, { color: "#fff", autoAlpha: 1 }, 0);
-  } else if (e.type === "mouseleave") {
-    // fade out images
-    // all links back to black
-    // change background color back to default
-
-    const tl = gsap.timeline({
-      onStart: () => updateBodyColor("#ACB7AE"),
-    });
-    tl.to([largeImage, smallImage], { autoAlpha: 0 }).to(
-      allLinks,
-      { color: "#000000", autoAlpha: 1 },
-      0
-    );
-  }
-}
-
-function createPortfolioMove(e) {
-  const { clientY } = e;
-
-  // move large image
-  gsap.to(largeImage, {
-    duration: 1.2,
-    y: getPortfolioOffset(clientY) / 5,
-    ease: "power3.out",
-  });
-
-  // move small image
-  gsap.to(smallImage, {
-    duration: 1.5,
-    y: -getPortfolioOffset(clientY) / 3,
-    ease: "power3.out",
-  });
-}
-
-function getPortfolioOffset(clientY) {
-  return -(select(".portfolio__categories").clientHeight - clientY);
-}
-
 // Parallax Images
 function initImageParallax() {
   // select all sections .with-parallax
@@ -529,7 +405,6 @@ function initScrollTo() {
 
     link.addEventListener("click", (e) => {
       e.preventDefault();
-      console.log(select(target));
       bodyScrollBar.scrollIntoView(select(target), {
         damping: 0.07,
         offsetTop: 100,
