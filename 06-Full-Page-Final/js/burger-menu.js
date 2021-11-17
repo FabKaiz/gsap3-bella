46d96f88157c00147a54b153ab8ed9c9aa804a27
Fabien Chareun <cacrfabfr@gmail.com>
2021-11-17 15:26:28 +0100

    counter menu

--- 06-Full-Page-Final/js/burger-menu.js
+++ 06-Full-Page-Final/js/burger-menu.js
@@ -1,13 +1,10 @@
-
-
 const initBurger = () => {
 
-  const logo               = document.querySelector('.logo');
-  const mainNav            = document.querySelector('.main-nav');
-  const viewport           = document.querySelector('#viewport');
   const burger             = document.querySelector('.burger-menu');
   const burgerNav          = document.querySelector('.burger-nav');
   const burgerNavContainer = document.querySelector('.burger-nav--container');
+  const splitText          = document.querySelectorAll('.split-text');
+  const menuCounterAfter = document.querySelectorAll('.menu-item');
 
 
   burger.addEventListener('click', () => {
@@ -33,10 +30,16 @@ const initBurger = () => {
         ease: "power1.inOut",
       },
     });
+
     tl.set(burgerNav, { duration: 0.1, autoAlpha: 1, display: 'block' })
       .to(burgerNavContainer, { duration: 0.1, autoAlpha: 1, display: 'block' }, 0)
       .fromTo(burgerNav, { yPercent: -100 }, { yPercent: 0 }, 0)
-      .to(burgerNav, {duration: 2, ease: "power4.out", backgroundSize: "100% 100%" }, 0.7)
+      .to(burgerNav, {duration: 2, ease: "power4.out", backgroundSize: "100% 100%" }, 0.6)
+      // Animate the navigation links
+      .to(splitText, {duration: 0.8, y: '0%', stagger: 0.1, autoAlpha: 1 }, 0.3)
+      // Move and show the counter ::after element
+      .to(menuCounterAfter, {duration: 0.5, stagger: 0.1, '--padding-left': '-13vw', '--counter-opacity': 0.5 }, 0.8)
+
     return tl;
 
   }
@@ -50,15 +53,33 @@ const initBurger = () => {
         ease: "power1.inOut",
       },
     });
+
     tl.to(burgerNav, { yPercent: 100 })
       .to(burgerNav, { duration: 0.1, autoAlpha: 0 }, 0.7)
       .to(burgerNavContainer, { duration: 0.1, autoAlpha: 0, display: 'none' }, 0.7)
-      .to(burgerNav, { duration: 0.1, ease: "power4.easeOut", backgroundSize: "130% 130%" }, 0.7)
+      .to(burgerNav, {
+        duration: 0.1,
+        ease: "power4.easeOut",
+        backgroundSize: "130% 130%"
+      }, 0.7)
+      // Animate the navigation links back to normal
+      .to(splitText, {
+        duration: 0.1,
+        y: '80%',
+        autoAlpha: 0
+      }, 0.7)
+      // hide and move back the counter ::after element
+      .to(menuCounterAfter, {
+        duration: 0.1,
+        stagger: 0.1,
+        '--padding-left': '-23vw',
+        '--counter-opacity': 0
+      }, 0.7)
+
     return tl;
 
   }
 
 };
-
 
 export { initBurger };
